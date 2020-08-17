d9c9e6e30b118e2b7b118faa260b37ef7be5a513
Amit1106 <69479586+Amit1106@users.noreply.github.com>
2020-08-17 12:05:11 +1100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    $to = "amitpatil.ap1106@gmail.com";
+    $subject = "Response from website";
+    $message = "I am very thankful to you";
+    $headers = "From: swapnilpatil.sp1106@gmail.com";
+
+    if(mail($to, $subject, $message, $headers)){
+        echo "mail send sucessfully";
+    }
+    else{
+        echo "can not send mail";
+    }
+
+
+}
+
+?>
